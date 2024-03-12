b3d1ffdb0cad5f5a88d87ceb5601df46b0575aba
faturoti <abfat049@gmail.com>
2024-03-12 15:19:53 -0700

    SOlution 2

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,14 +1,38 @@
 #!/usr/bin/python3
+
+"""Test file for Base Model class
+"""
+
+import os
+import re
+import json
+import uuid
+import unittest
+from time import sleep
+from datetime import datetime
 from models.base_model import BaseModel
 
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+class TestBaseModel(unittest.TestCase):
+    """
+    Tests attributes of the base model
+    """
+    def setUp(self):
+        """
+        Set up function for unit test
+        """
+        pass
+
+    def test_basic(self):
+        """
+        Test for basic stuff
+        """
+        my_model = BaseModel()
+        my_model2 = BaseModel()
+        self.assertNotEqual(my_model.id, my_model2.id)
+
+    def test_datetime(self):
+        """
+        Test for created and updated time
+        """
+        mymodel1 = BaseModel()
+        self.assertNotEqual(mymodel1.created_at, mymodel1.updated_at)
