e2d26fd3d703f53ef44b67d87a404945e6188ada
faturoti <abfat049@gmail.com>
2024-03-12 16:49:10 -0700

    SOlution 2

---
+++ tests/test_place.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This is to test for different places
+"""
+
+import os
+import models
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+
+
+class TestPlace(unittest.TestCase):
